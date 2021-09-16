135ea915e5feaa20283cfd18fa210c2395513947
Łukasz Kaźmierski <prywatnaapka@gmail.com>
2021-09-16 21:58:57 +0200

    fix: remove import

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from 'next';
 import TodoListView from '../../components/todo-list-view/todo-list-view';
 import NavigationBar from '../../components/navigation-bar/navigation-bar';
 import React from 'react';

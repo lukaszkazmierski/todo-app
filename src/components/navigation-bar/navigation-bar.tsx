7a9eee2f4bb88c841f7fe35616abf96a7a1059a7
Łukasz Kaźmierski <prywatnaapka@gmail.com>
2021-09-16 21:56:16 +0200

    feat: update navigation bar using material core

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -1,39 +1,18 @@
 import { NextPage } from 'next';
 import 'bootstrap/dist/css/bootstrap.css';
+import { AppBar, Box, IconButton, Toolbar , Typography, Button } from '@material-ui/core';
 
 const NavigationBar: NextPage = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <a className="navbar-brand text-light px-3" href="">
-        Todo App
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link text-light" href="">
-              Todos
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-light" href="">
-              Random Todos
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar className="bg-primary" position="static">
+        <Toolbar>
+          <Typography className="ms-2" variant="h6" component="div" >
+            Todo App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 

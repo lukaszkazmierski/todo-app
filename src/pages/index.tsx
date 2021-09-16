0938901d6f1cd6f100a57e94372dddd67c677bf9
Łukasz Kaźmierski <prywatnaapka@gmail.com>
2021-09-16 22:54:36 +0200

    merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,4 @@
 import type { NextPage } from 'next';
-<<<<<<< HEAD
-const Home: NextPage = () => {
-  return (
-    <h1>TODO-APP</h1>
-  );
-};
-
-export default Home;
-=======
 import NavigationBar from '../components/navigation-bar/navigation-bar';
 import HomePage from './home-page/home-page';
 const MainPage: NextPage = () => {
@@ -15,4 +6,3 @@ const MainPage: NextPage = () => {
 };
 
 export default MainPage;
->>>>>>> feat/basic-todo-functionality
